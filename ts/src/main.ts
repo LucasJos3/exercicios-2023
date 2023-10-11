0fc52986e9060ea02a50bbc3cb34ce81e02d0f93
LucasJos3 <lucasjosedepaula@hotmail.com>
2023-10-11 12:16:51 -0300

    projeto

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -8,5 +8,27 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+const botao = document.getElementById('btn-create-topic');
+const botao2 = document.getElementById('btn-show-more');
+
+botao?.addEventListener('click', fnform);
+
+function fnform() {
+  return alert();
+}
+
+botao2?.addEventListener('click', vermais);
+
+function vermais() {
+  let pontos = document.getElementById('pontos');
+  let maisTexto = document.getElementById('mais');
+  let bnt = document.getElementById('btn-show-more');
+
+  if (pontos?.style.display === 'none') {
+    pontos.style.display = 'inline';
+  }
+}
